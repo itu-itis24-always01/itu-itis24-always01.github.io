2bc51406a2d665447c9e6bdd5b2dde4daabeae13
itu-itis24-always01 <aliyev24@itu.edu.tr>
2025-01-18 23:28:49 +0300

    Added a restart button, a point system giving 20 points to users for correct answer, and heart system for taking one heart of the user per incorrect answer

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-const playersubmit = document.getElementById("submit");
-const playerprediction = document.geteElementById("prediction");
-
-
-
+const playerSubmit = document.getElementById("submit");
+const playerInput = document.getElementById("prediction");
+const playerScore = document.getElementById("score");
+const playerRestartBu
